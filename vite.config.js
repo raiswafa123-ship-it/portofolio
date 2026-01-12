98edd92cd8abd30517fd31cd4961dd3e6f4e3288
dmz_ntz <raiswafa123@gmail.com>
2026-01-12 15:15:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "portofolio/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist"
   }
 });
+
 
 
